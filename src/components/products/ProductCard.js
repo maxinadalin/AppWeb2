45fd7d4cdccd4ada56e7fea7fa53fc63349e3f23
maximiliano nadalin <maximilianoenadalin@gmail.com>
2024-05-13 23:30:41 +0200

    actualizacion

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -9,11 +9,13 @@ const ProCard = ({ producto, get_products }) => {
 
   const subcategory = params.subcategoryid;
 
+
   useEffect(() => {
     get_products();
   }, []);
 
   console.log("Subcategoría:", subcategory);
+
 
   return (
     <>
@@ -22,9 +24,10 @@ const ProCard = ({ producto, get_products }) => {
         producto !== undefined &&
         producto.map((item) => {
             console.log("item.category:", item.category);
-          if (item.category && item.category === subcategory) { 
+          if (item.category === parseInt(subcategory)) { 
             return (
-              <div key={item.id} className="group relative mx-2">
+                <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                <div key={item.id} className="group relative mx-2">
                 <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
                   <img
                     src={item.photo}
@@ -35,14 +38,19 @@ const ProCard = ({ producto, get_products }) => {
                 <div className="mt-4 flex justify-between">
                   <div>
                     <h3 className="text-sm text-gray-700">
-                      <Link to={`/ProductDetail/${item.id}`}>
+                    <Link to={`/ProductDetail/${item.id}`}>
                         <span aria-hidden="true" className="absolute inset-0" />
                         {item.name}
                       </Link>
                     </h3>
                   </div>
+                  <p className="text-sm font-medium text-gray-900">${item.price}</p>
                 </div>
               </div>
+              </div>
+              
+
+              
             );
           }
         })}
